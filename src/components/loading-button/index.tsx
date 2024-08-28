4b01f2e8af7a08e5e8122b71fbb5940d72a35382
Naza Zulfiqi <zulfiqinaza@gmail.com>
2024-08-28 13:36:05 +0700

    fix: error in build, remove package unused

--- src/components/loading-button/index.tsx
+++ src/components/loading-button/index.tsx
@@ -1,5 +1,4 @@
 import { Loader2 } from "lucide-react";
-import React from "react";
 
 import { Button, ButtonProps } from "@/components/ui/button";
 
--- src/modules/add-user/module.tsx
+++ src/modules/add-user/module.tsx
@@ -21,7 +21,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
