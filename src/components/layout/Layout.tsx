5764b1d40db54ffb34421c0c6d67adb64d4aa221
Naza Zulfiqi <zulfiqinaza@gmail.com>
2024-08-28 21:50:28 +0700

    feat: dark background image

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,8 +3,9 @@ import { ModeToggle } from '../mode-toggle';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="bg-white dark:bg-slate-950 ">
-      <div className="mx-auto max-w-7xl py-12 px-4 md:px-8 lg:px-12">
+    <main className="relative min-h-screen bg-white dark:bg-slate-950">
+      <div className="absolute inset-0 dark:bg-[url('/LooperGroup2.png')] dark:bg-no-repeat dark:bg-cover" />
+      <div className="relative mx-auto max-w-7xl py-12 px-4 md:px-8 lg:px-12">
         <h1 className="lg:text-3xl text-2xl text-center pb-8 font-bold">
           OBS Frontend Assignment - Naza Zulfiqi
         </h1>
